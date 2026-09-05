773d9b5a5ccfe0e9b8c1c38c8d4c8084e82b7301
Aparajit Garg <aparajit.garg06@gmail.com>
2026-09-05 22:25:22 +0530

    feat: test and theme setup done

---
+++ packages/shared-ui/src/ThemeContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import { Theme } from "./theme"
+
+type ThemeContextValue = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextValue | null>(null);
+
+type ThemeProviderProps = {
+  children: ReactNode,
+  defaultTheme?: Theme
+}
+
+const ThemeProvider = ({ children, defaultTheme = "light" }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const toggleTheme = () => {
+    setTheme(current => current === "light" ? "dark" : "light");
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, setTheme }}>
+      <div data-theme={theme} className="contents">
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = (): ThemeContextValue => {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  return context;
+}
+
+export default ThemeProvider;
